d8c23e9d24c02603799f5cc42cc68d3b390e20eb
Kyle Jorgensen <kylewjorgensen@gmail.com>
2013-12-06 02:30:42 -0800

    initial commit

---
+++ countIPdata.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+
+# Kyle Jorgensen, CS284
+# Script to take in the destination IP addresses from our capture data, then 
+#   strip the URL's that have long CDN names, and then count the overall 
+#   occurences and store them in a dictionary. Once they are organized by 
+#
+#   < key: top-level-domain-name, value: count-of-occurences >
+#
+#   then we output this data to another file.
+
+import sys
+import operator
+from collections import defaultdict
+
+def readToDictionary(infile):
+    '''
+    Takes a text file of all the occurences of destination IP addresses. It adds these to a dictionary where each key is the URL and the value is the count of how many times it appears in the file.
+    '''
+
+    ip_dict = defaultdict(int)
+    ip_data_file = open(infile,'r')
+
+    for line in ip_data_file:
+
+        # if it's one of the weird cases with two IP's separated with a comma 
+        if ',' in line:
+            for item in line.split(','):
+                ip_dict[item.strip()] += 1
+        else:
+            #otherwise, just add it
+            ip_dict[line.strip()] += 1
+
+    ip_data_file.close()
+
+    return ip_dict
+
+
+def sortAndShortenURLs(ip_dict, precision):
+    '''
+    Takes a dictionary with our IP data, and returns a list of tuples of shortened URLs and the count of how many times they appear. The final result is sorted by count, in decreasing order.
+
+    Precision is the amount that we want to shorten URLs. For example with precision 3, "csil.cs.ucsb.edu" becomes "*.cs.ucsb.edu"
+    '''
+    
+    numbers = ['1','2','3','4','5','6','7','8','9','0']
+    
+    for k, v in ip_dict.items():        
+        if k[0] not in numbers:
+            # it is a URL with name resolution, shorten it
+            url_list = k.split(".")
+            
+            # with precision=2, "stuff.website.com" will become "website.com" 
+            new_entry = url_list[-(precision):]
+            new_entry = ["*"] + new_entry
+
+            # remove the old, long url from the dictionary
+            del ip_dict[k]
+
+            # add the new, short version
+            ip_dict[".".join(new_entry)] = v
+
+    return sorted(ip_dict.items(), key=operator.itemgetter(1), reverse=True)
+
+def writeOutput(outfilePath, tuples):
+    outfile = open(outfilePath, 'w')
+    
+    for k, v in tuples:
+        outfile.write(k + " " + str(v) + "\n")
+        
+    outfile.close()
+
+#####################################################
+
+
+if len(sys.argv) < 4:
+    print "Usage: python countIPdata.py <inputfile> <outputfile> <precision> \n"
+else:
+    current = "/cs/student/kylejorgensen/cs284/"
+    filename = current + sys.argv[1]
+    outputFile = current + sys.argv[2]
+    
+    data = readToDictionary(filename)
+    sorted_data = sortAndShortenURLs(data,int(sys.argv[3]))
+    writeOutput(outputFile, sorted_data)
+    
+    print "*** Finished! You can find your data in " + outputFile +'\n'
+
+    
+
+
